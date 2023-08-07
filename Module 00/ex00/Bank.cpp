3bac6c8f2ff2cb60a04dd786e4e0832875112f3f
Alexander Israyelyan <aisrayelyan@aligntech.com>
2023-08-07 19:16:24 +0400

    (+) Implement more features for ex00

--- Module 00/ex00/Bank.cpp
+++ Module 00/ex00/Bank.cpp
@@ -4,6 +4,8 @@ int Bank::Account::count = 0;
 
 Bank::Account::Account() : id(count++), value(0) {}
 
+Bank::Account::Account(int value) : id(count++), value(value) {}
+
 int Bank::Account::get_id() const
 {
 	return (id);
@@ -14,5 +16,13 @@ int Bank::Account::get_value() const
 	return (value);
 }
 
+std::ostream &operator<<(std::ostream &stream, const Bank::Account &account)
+{
+	stream << "[" << account.get_id() << "] - [" << account.get_value() << "]";
+	return (stream);
+}
+
 Bank::Bank() : liquidity(0), client_accounts() {}
+
+Bank::Bank(int liquidity) : liquidity(liquidity), client_accounts() {}
 
--- Module 00/ex00/Bank.hpp
+++ Module 00/ex00/Bank.hpp
@@ -1,28 +1,38 @@
 #ifndef BANK_HPP
 # define BANK_HPP
 
+# include <iostream>
 # include <map>
 
 struct Bank
 {
-	private:
+	public:
 		struct Account
 		{
 			private:
 				int id;
 				int value;
 				static int count;
-			public:
 				Account();
+				Account(int value);
+			public:
 				int get_id() const;
 				int get_value() const;
+				friend class Bank;
 		};
-
+	private:
 		int liquidity;
 		std::map<int, Account*> client_accounts;
+		Bank(const Bank&);
+		Bank &operator=(const Bank&);
 	public:
 		Bank();
+		Bank(int liquidity);
+		~Bank();
 		const Account &operator[](int id) const;
+		
 };
+
+std::ostream &operator<<(std::ostream &stream, const Bank::Account &account);
 
 #endif
